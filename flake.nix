66b4e38c7ff3a7a08ad99ba02eea43f64e48e6a2
Fredrik Enestad <fredrik@enestad.com>
2024-10-14 13:35:17 +0200

    initial flake for encore released binaries

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "nix flake that wraps the released encore binaries";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+      ];
+    in
+    {
+      packages = eachSystem (system: {
+        encore = nixpkgs.legacyPackages.${system}.callPackage ./encore.nix { };
+      });
+    };
+}
