fb5232980927808ed16778a50ddd330601598359
Fredrik Enestad <fredrik@enestad.com>
2024-10-24 14:16:08 +0200

    update to 1.42.1

--- release.nix
+++ release.nix
@@ -1,10 +1,10 @@
 {
-  version = "1.41.14";
+  version = "1.42.1";
 
   checksums = {
-    "darwin_arm64" = "8338520f00daadc62b56e38c990325ad0b9513c342bb732a1ed2dc6e94b858df";
-    "darwin_amd64" = "8389def8ed3948a968c817fc2c0db46fd32642a1e621e6d8a2eb3308c01eb6ec";
-    "linux_arm64" = "5ee62d387dec6772e39e4434ec3c58c162736a97703a82e95f11ef574b9d54f6";
-    "linux_amd64" = "c5674b540d7272faba3cbe4d71c11dddadc261b0d0eb0cffb384fc49fa88153f";
+    "darwin_arm64" = "ab745eeb690d67e5e10490f8ef2a15dcc5a503295bfc5e972c1f165927f215a7";
+    "darwin_amd64" = "f88c9ee17bf1c2b78a0d2e737f84611ae760a8a423e6416e75fde7412ff37e41";
+    "linux_arm64" = "a1753fb346bcc0fd2c0d2fe9dbe585180890d4c1fb595702bb89b9cda432eef7";
+    "linux_amd64" = "2d4174e625691b42e52dbf2cef6457f0b28dedf7fdaf4c4c54d0e5745d151885";
   };
 }
