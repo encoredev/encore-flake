239d189d5cb29301b2f2f1fab27691d5aa41e8d5
André Eriksson <eandre@gmail.com>
2024-10-28 16:08:19 +0100

    Release Encore v1.42.4-develop+nixflake
    This commit bumps the Encore version to v1.42.4-develop+nixflake
    
    _This commit was created by the Encore release process automatically._

--- release.nix
+++ release.nix
@@ -1,10 +1,11 @@
+# Auto-generated file, do not edit
 {
-  version = "1.42.1";
+  version = "1.42.4-develop+nixflake";
 
   checksums = {
-    "darwin_arm64" = "ab745eeb690d67e5e10490f8ef2a15dcc5a503295bfc5e972c1f165927f215a7";
-    "darwin_amd64" = "f88c9ee17bf1c2b78a0d2e737f84611ae760a8a423e6416e75fde7412ff37e41";
-    "linux_arm64" = "a1753fb346bcc0fd2c0d2fe9dbe585180890d4c1fb595702bb89b9cda432eef7";
-    "linux_amd64" = "2d4174e625691b42e52dbf2cef6457f0b28dedf7fdaf4c4c54d0e5745d151885";
+    "darwin_arm64" = "fdbc7d0f66da0b959b2227f82710f4211623b0d4bf2ad46fd2a77f1101c5dbe9";
+    "darwin_amd64" = "2197b3f7c0e16ec44bb2d3201797fafa50d7b3a77fd7630f543425fdccc2cbb6";
+    "linux_arm64" = "00a8091a2e82715a8975db68549fdbfe646afc134701711058cace9dcc632f5d";
+    "linux_amd64" = "f60030bfa370e22ad2481fea0f128bb0cacf701c26f0b007f3c359aa010492d5";
   };
 }
