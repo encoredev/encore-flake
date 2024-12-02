5d434b2348e4a2f66935e7738a0f7833dde5c401
André Eriksson <eandre@gmail.com>
2024-12-02 14:31:37 +0100

    Release Encore v1.44.8
    This commit bumps the Encore version to v1.44.8
    
    _This commit was created by the Encore release process automatically._

--- release.nix
+++ release.nix
@@ -1,11 +1,11 @@
 # Auto-generated file, do not edit
 {
-  version = "1.44.5";
+  version = "1.44.8";
 
   checksums = {
-    "darwin_arm64" = "f8e6af6903df2eec85fba75a502ea00576d2740e8fbd63556ba633f47dc9d1d1";
-    "darwin_amd64" = "9500f79a2d66a8f220c1f7e4e5a8b1daceb64b9aa6c691ac0d4954e67369b9a0";
-    "linux_arm64" = "1b2bce23c5b899b78bf6a7cc877dd94f5e3e830a72027ad11da76c8d200278e4";
-    "linux_amd64" = "6fdc11821e9ba9b2112486e45bebc28f93eb9ce2ed5b98cb7249f2d40200ae31";
+    "darwin_arm64" = "0991027d77a57e39da8bde73456d05f1bd69fdb54a2f75de5ed6ba7f7b0276e5";
+    "darwin_amd64" = "325214c2c91afd11b5bdf067645d29a199900f79a91d1f8e1b4dcba7641f491a";
+    "linux_arm64" = "943da299552afc29ec7207c6fbbcdffca71bd7cf40be0ba63a152b431b736e81";
+    "linux_amd64" = "a9a16ff142bb467b5cc310f386bf186ff91695264cb30aa446ac88527e765045";
   };
 }
